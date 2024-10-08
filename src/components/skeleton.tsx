ae23e8b3c57fb16bc5c336631b99600461965ca1
Lars Berger <lars.berger@yahoo.com>
2024-10-08 21:43:35 +0800

    style: format with prettier

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,13 +1,13 @@
-import { cn } from "@/libs/cn";
-import { type ComponentProps, splitProps } from "solid-js";
+import { cn } from '~/utils';
+import { type ComponentProps, splitProps } from 'solid-js';
 
-export const Skeleton = (props: ComponentProps<"div">) => {
-	const [local, rest] = splitProps(props, ["class"]);
+export const Skeleton = (props: ComponentProps<'div'>) => {
+  const [local, rest] = splitProps(props, ['class']);
 
-	return (
-		<div
-			class={cn("animate-pulse rounded-md bg-primary/10", local.class)}
-			{...rest}
-		/>
-	);
+  return (
+    <div
+      class={cn('animate-pulse rounded-md bg-primary/10', local.class)}
+      {...rest}
+    />
+  );
 };
