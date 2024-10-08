d6e564c2d242055be51209e3fd575065acbcc368
Lars Berger <lars.berger@yahoo.com>
2024-10-08 20:49:11 +0800

    feat: use own `clsx` implementation

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,44 @@
-import type { ClassValue } from 'clsx';
-import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export const cn = (...classLists: ClassValue[]) =>
-  twMerge(clsx(classLists));
+export const cn = (...classValues: ClassValue[]) =>
+  twMerge(mergeClasses(classValues));
+
+type ClassValue =
+  | string
+  | string[]
+  | Record<string, boolean>
+  | null
+  | undefined;
+
+/**
+ * Utility for constructing `class` names conditionally.
+ *
+ * Inspired by `clsx` https://github.com/lukeed/clsx.
+ */
+function mergeClasses(...classValues: ClassValue[]): string {
+  let classString = '';
+
+  for (const classValue of classValues) {
+    if (classValue === null || classValue === undefined) {
+      continue;
+    }
+
+    if (typeof classValue === 'string') {
+      classString += `${classValue} `;
+    }
+
+    if (Array.isArray(classValue)) {
+      classValue.forEach(inputPart => (classString += `${inputPart} `));
+    }
+
+    if (typeof classValue === 'object') {
+      for (const [key, val] of Object.entries(classValue)) {
+        if (!!val) {
+          classString += `${key} `;
+        }
+      }
+    }
+  }
+
+  return classString;
+}
