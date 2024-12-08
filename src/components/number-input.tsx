6f804f2afa36c233038f77333e98ecd5c06c714b
lars-berger <lars.berger@yahoo.com>
2024-12-09 04:17:56 +0800

    feat: move increment/decrement triggers on number field to the right-side

--- src/components/number-input.tsx
+++ src/components/number-input.tsx
@@ -11,6 +11,7 @@ import {
   makeFieldComponent,
   type FormInputProps,
 } from './forms';
+import { IconMinus, IconPlus } from '@tabler/icons-solidjs';
 
 export type NumberFieldProps = ComponentProps<typeof NumberField>;
 
@@ -51,54 +52,19 @@ export function NumberInput(props: NumberInputProps) {
         placeholder={local.placeholder}
         onChange={e => local.onChange?.(e.currentTarget.valueAsNumber)}
         class={
-          'flex h-9 w-full rounded-md border border-input bg-transparent px-10 py-1 text-center text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50'
+          'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-left text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50'
         }
         onBlur={() => local.onBlur?.()}
       />
 
-      <_NumberField.DecrementTrigger
-        class={
-          'absolute left-0 top-1/2 -translate-y-1/2 p-3 disabled:cursor-not-allowed disabled:opacity-20'
-        }
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="size-4"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M5 12h14"
-          />
-          <title>Decreasing number</title>
-        </svg>
-      </_NumberField.DecrementTrigger>
-
-      <_NumberField.IncrementTrigger
-        class={
-          'absolute right-0 top-1/2 -translate-y-1/2 p-3 disabled:cursor-not-allowed disabled:opacity-20'
-        }
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="size-4"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 5v14m-7-7h14"
-          />
-          <title>Increase number</title>
-        </svg>
-      </_NumberField.IncrementTrigger>
+      <div class="absolute right-0 top-0 flex h-full w-8 flex-col divide-y divide-input border border-input bg-muted rounded-r-md">
+        <_NumberField.IncrementTrigger class="flex h-1/2 items-center justify-center hover:bg-muted-foreground/10">
+          <IconPlus class="size-3" />
+        </_NumberField.IncrementTrigger>
+        <_NumberField.DecrementTrigger class="flex h-1/2 items-center justify-center hover:bg-muted-foreground/10">
+          <IconMinus class="size-3" />
+        </_NumberField.DecrementTrigger>
+      </div>
     </_NumberField>
   );
 }
